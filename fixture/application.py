e2fafc3a8040ad84aa8aed7fe1d786289ac46971
Alexander <berpress@gmail.com>
2019-10-27 21:00:15 +0300

    Add pre commit hook

--- fixture/application.py
+++ fixture/application.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from fixture.login_page import LoginPage
 
+
+
 class Application:
     def __init__(self, base_url):
         self.wd = webdriver.Chrome(ChromeDriverManager().install())
