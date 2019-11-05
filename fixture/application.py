5bdba1f384940830bdded3424972c4eb85c078f9
Alexander <berpress@gmail.com>
2019-11-05 09:29:55 +0300

    Delete implicitly_wait

--- fixture/application.py
+++ fixture/application.py
@@ -6,7 +6,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 class Application:
     def __init__(self, base_url):
         self.wd = webdriver.Chrome(ChromeDriverManager().install())
-        self.wd.implicitly_wait(20)
         self.login = LoginPage(self)
         self.base_url = base_url
 
