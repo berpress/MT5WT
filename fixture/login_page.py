3b985a1443cbfe445fd996f7aeb6a0fb2861654d
Alexander <berpress@gmail.com>
2019-11-05 09:18:17 +0300

    Linter Fix

--- fixture/login_page.py
+++ fixture/login_page.py
@@ -1,8 +1,3 @@
-
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-
 class LoginPage:
     def __init__(self, app):
         self.app = app
