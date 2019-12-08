8caff6e3375a24a4b28773ea42aa86b8a8562dd6
berpress <berpress@gmail.com>
2019-12-08 09:35:31 +0400

    Add pytest.issue

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,10 @@
+import pytest
+
 from common.common import NETTING_ACCOUNT
 
 
 class TestLogin:
+    @pytest.mark.issue(id="1")
     def test_login(self, app):
         app.auth.open()
         app.auth.auth_terminal(NETTING_ACCOUNT.login, NETTING_ACCOUNT.password)
